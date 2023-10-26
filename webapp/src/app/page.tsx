b3f1712fd2d456fab50ea02707cb44ba2ae414b7
{W}Pots <12676504+wpots@users.noreply.github.com>
2023-10-26 12:21:49 +0200

    get it working now, make it pretty later

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useRef } from "react";
+// import { AlphaTabApi } from "@coderline/alphatab";
+import Script from "next/script";
 
 export default function Home() {
+  const tab = useRef(null);
+  const wrap = useRef(null);
+  const handleScriptLoaded = () => {
+    if (tab.current) {
+      const settings = {
+        file: "https://www.alphatab.net/files/canon.gp",
+      };
+      const alphaTabInstance = new window.alphaTab.AlphaTabApi(tab.current, settings);
+      // alphaTabInstance.file = "./radio.xml";
+      alphaTabInstance.scoreLoaded?.on(score => console.log("loaded", score));
+    }
+  };
+  useEffect(() => {
+    console.log("Well something happened", tab.current);
+    tab?.current.addEventListener("alphaTab.scoreLoaded", e => {
+      console.log("Score was loaded!", e.detail);
+    });
+  }, [tab]);
+  if (!tab) {
+    return <main>...loading</main>;
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@coderline/alphatab@latest/dist/alphaTab.min.js"
+        onLoad={handleScriptLoaded}
+      />
+      <header>
+        <h1>Lady Sunday</h1>
+        <small>omdat mijn moeder mij verkeerd verstond...</small>
+      </header>
+      <section className="at-wrap" ref={wrap}>
+        <div className="at-content">
+          <div className="at-sidebar">Tracker</div>
+          <div className="at-viewport">
+            <div className="at-main" ref={tab}></div>
+          </div>
+          <div className="at-controls">Controls</div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }

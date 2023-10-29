7aee59a2ef97b6dd223de773675be1c9a3a6a1d7
{W}Pots <12676504+wpots@users.noreply.github.com>
2023-10-29 13:18:23 +0100

    selectable songs

--- webapp/src/app/_components/AlphaTab/SourceControls.tsx
+++ webapp/src/app/_components/AlphaTab/SourceControls.tsx
@@ -1,14 +1,33 @@
 "use client";
 
+import { AlphaTabContext } from "@/app/_store/alphaTab-context";
 import { Button, Space } from "antd";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 export default function SourceControls() {
+  const [songSelected, setSongSelected] = useState(1);
+  const { apiInstance } = useContext(AlphaTabContext);
+  const handleSongSelected = song => {
+    console.log(song);
+    if (apiInstance) apiInstance.load(song.file);
+    setSongSelected(song);
+  };
 
   return (
     <Space>
-      {["Radio", "Maria", "Suddenly I See", "You Know I'm No Good"].map((song: string) => (
-        <Button key={song}>{song}</Button>
+      {[
+        { id: 0, name: "Radio", file: "./radio.xml" },
+        { id: 1, name: "Maria", file: "./maria.gp5" },
+        { id: 2, name: "Suddenly I See", file: "./suddenly.gp4" },
+        { id: 3, name: "You Know I'm No Good", file: "./nogood.gp5" },
+      ].map((song: Record<any, any>) => (
+        <Button
+          onClick={() => handleSongSelected(song)}
+          key={song.id}
+          className={songSelected.id === song.id ? "active" : undefined}
+        >
+          {song.name}
+        </Button>
       ))}
     </Space>
   );
